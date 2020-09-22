17a722cfa37769fa0c46ebf812c1fcba199db964
Afon <hcls70810@gmail.com>
2020-09-23 01:27:24 +0300

    Initial commit

---
+++ send_mail.py
@@ -0,0 +1,22 @@
+from email import message
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_mail(customer, dealer, rating, comments):
+    port = 2525
+    smtp_server = 'smtp.mailtrap.io'
+    login = '7cd8fe24651c40'
+    password = '42879c25530dab'
+    message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
+
+    sender_email = 'email1@example.com'
+    receiver_email = 'eamil2@example.com'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'Lexus Feedback'
+    msg['From'] = sender_email
+    msg['to'] = receiver_email
+
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
